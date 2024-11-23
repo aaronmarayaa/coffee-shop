4487798cee66a951a8847111b8546bf6a2ca2039
Aaron Maraya <aaronmaraya7@gmail.com>
2024-11-24 07:46:15 +0800

    Modify review section

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,64 +9,24 @@ function toggleMenu() {
         burger.innerHTML = '&#9776;';
     }
 }
-document.addEventListener("scroll", () => {
-    const navbar = document.getElementById("navMenu");
 
-    // Get the vertical scroll position
-    const scrollPosition = window.scrollY;
+let home = document.getElementById('home')
+let about = document.getElementById('about')
+let shop = document.getElementById('shop')
+let reviews = document.getElementById('reviews')
+let contacts = document.getElementById('contacts')
 
-    // Determine the color based on the section in view
-    if (scrollPosition < document.getElementById("home").offsetTop) {
-      navbar.style.color = "rgba(0, 0, 255, 0.7)"; // Home
-    } else if (scrollPosition < document.getElementById("about").offsetTop) {
-      navbar.style.color = "rgba(0, 128, 0, 0.7)"; // Shop
-    } else {
-      navbar.style.color = "rgba(255, 0, 0, 0.7)"; // About
-    }
-});
-
-
-
-let home = document.getElementById('navhome')
-let about = document.getElementById('navabout')
-let shop = document.getElementById('navshop')
-let reviews = document.getElementById('navreviews')
-let contacts = document.getElementById('navcontacts')
-
-
-function activeHome() {
-    home.style.color = "#A64B2A"
-    about.style.color = "#FFEBC1"
-    shop.style.color = "#FFEBC1"
-    reviews.style.color = "#FFEBC1"
-    contacts.style.color = "#FFEBC1"
-    
-}
-function activeAbout() {
-    about.style.color = "#A64B2A"
-    home.style.color = "#FFEBC1"
-    shop.style.color = "#FFEBC1"
-    reviews.style.color = "#FFEBC1"
-    contacts.style.color = "#FFEBC1"
-}
 function activeShop() {
-    shop.style.color = "#A64B2A"
-    about.style.color = "#FFEBC1"
-    home.style.color = "#FFEBC1"
-    reviews.style.color = "#FFEBC1"
-    contacts.style.color = "#FFEBC1"
+    shop.style.display = "block"
+    about.style.display = "none"
+    home.style.display = "none"
+    reviews.style.display = "none"
+    contacts.style.display = "none"
 }
-function activeReviews() {
-    reviews.style.color = "#A64B2A"
-    about.style.color = "#FFEBC1"
-    shop.style.color = "#FFEBC1"
-    home.style.color = "#FFEBC1"
-    contacts.style.color = "#FFEBC1"
-}
-function activeContacts() {
-    contacts.style.color = "#A64B2A"
-    about.style.color = "#FFEBC1"
-    shop.style.color = "#FFEBC1"
-    reviews.style.color = "#FFEBC1"
-    home.style.color = "#FFEBC1"
+function back() {
+    shop.style.display = "none"
+    about.style.display = "block"
+    home.style.display = "block"
+    reviews.style.display = "block"
+    contacts.style.display = "block"
 }
