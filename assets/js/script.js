11c2f0ff4dcd2fff99c3dbfdad060fac524119c2
Aaron Maraya <aaronmaraya7@gmail.com>
2024-11-26 20:43:19 +0800

    Add login page

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,56 @@ let about = document.getElementById('about')
 let shop = document.getElementById('shop')
 let reviews = document.getElementById('reviews')
 let contacts = document.getElementById('contacts')
+let navbar = document.getElementById('navbar')
+let main = document.getElementById('main')
+let login_ = document.getElementById('login')
+
+
+correctUsername = "admin"
+correctPassword = "admin"
+
+function signin() {
+    let username = document.getElementById('username')
+    let password = document.getElementById('password')
+    if(correctUsername == username.value.trim() && correctPassword == password.value.trim()) {
+        password.value = ""
+        return true
+    } else {
+        return false
+    }
+}
+
+function login(event) {
+    event.preventDefault(); 
+    if(signin()) {
+        main.style.display = "block"
+        navbar.style.display = "block"
+        login_.style.display = "none"
+    } else {
+        alert("Invalid Email or Password")
+        return false
+    }
+}
+
+function confirmLogout(event) {
+    event.preventDefault();
+
+    let confirm_ = confirm("Are you sure you want to log out?")
+    if (confirm_) {
+        main.style.display = "none"
+        navbar.style.display = "none"
+        login_.style.display = "flex"
+    }
+}
+
+function visiblePassword() {
+    const passwordField = document.getElementById("password");
+    if (passwordField.type === "password") {
+      passwordField.type = "text";
+    } else {
+      passwordField.type = "password";
+    }
+  }
 
 function activeShop() {
     shop.style.display = "block"
